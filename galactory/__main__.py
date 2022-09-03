871de2e1357729d5c36471b0331702551d4e9e6d
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-09-02 22:20:58 -0400

    add config file support, env vars

--- galactory/__main__.py
+++ galactory/__main__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # (c) 2022 Brian Scholer (@briantist)
 
-import os
 import logging
 from configargparse import ArgParser
 from artifactory import ArtifactoryPath
@@ -16,25 +15,28 @@ if __name__ == '__main__':
             'galactory is a partial Ansible Galaxy proxy that uploads and downloads collections, '
             'using an Artifactory generic repository as its backend.'
         ),
+        default_config_files=['/etc/galactory.d/*.conf', '~/.galactory/*.conf'],
     )
-    parser.add_argument('--listen-addr', default='0.0.0.0', type=str, help='The IP address to listen on.')
-    parser.add_argument('--listen-port', default=5555, type=int, help='The TCP port to listen on.')
-    parser.add_argument('--server-name', type=str, help='The host name and port of the server, as seen from clients. Used for generating links.')
-    parser.add_argument('--artifactory-path', type=str, required=True, help='The URL of the path in Artifactory where collections are stored.')
-    parser.add_argument('--artifactory-api-key', default=os.environ.get('GALACTORY_ARTIFACTORY_API_KEY'), help='If set, is the API key used to access Artifactory.')
-    parser.add_argument('--use-galaxy-key', action='store_true', help='If set, uses the Galaxy token as the Artifactory API key.')
-    parser.add_argument('--prefer-configured-key', action='store_true', help='If set, prefer the confgured Artifactory key over the Galaxy token.')
-    parser.add_argument('--log-file', type=str, help='If set, logging will go to this file instead of the console.')
+    parser.add_argument('-c', '--config', required=False, is_config_file=True, help='The path to a config file.')
+    parser.add_argument('--listen-addr', default='0.0.0.0', type=str, env_var='GALACTORY_LISTEN_ADDR', help='The IP address to listen on.')
+    parser.add_argument('--listen-port', default=5555, type=int, env_var='GALACTORY_LISTEN_PORT', help='The TCP port to listen on.')
+    parser.add_argument('--server-name', type=str, env_var='GALACTORY_SERVER_NAME', help='The host name and port of the server, as seen from clients. Used for generating links.')
+    parser.add_argument('--artifactory-path', type=str, required=True, env_var='GALACTORY_ARTIFACTORY_PATH', help='The URL of the path in Artifactory where collections are stored.')
+    parser.add_argument('--artifactory-api-key', type=str, env_var='GALACTORY_ARTIFACTORY_API_KEY', help='If set, is the API key used to access Artifactory.')
+    parser.add_argument('--use-galaxy-key', action='store_true', env_var='GALACTORY_USE_GALAXY_KEY', help='If set, uses the Galaxy token as the Artifactory API key.')
+    parser.add_argument('--prefer-configured-key', action='store_true', env_var='GALACTORY_PREFER_CONFIGURED_KEY', help='If set, prefer the confgured Artifactory key over the Galaxy token.')
+    parser.add_argument('--log-file', type=str, env_var='GALACTORY_LOG_FILE', help='If set, logging will go to this file instead of the console.')
     parser.add_argument(
         '--log-level',
         type=str.upper,
+        env_var='GALACTORY_LOG_LEVEL',
         default='INFO',
         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='The desired logging level.',
     )
-    parser.add_argument('--log-headers', action='store_true', help='Log the headers of every request (DEBUG level only).')
-    parser.add_argument('--log-body', action='store_true', help='Log the body of every request (DEBUG level only).')
-    parser.add_argument('--proxy-upstream', type=lambda x: str(x).rstrip('/') + '/', help='If set, then find, pull and cache results from the specified galaxy server in addition to local.')
-    parser.add_argument('-npns', '--no-proxy-namespace', action='append', default=[], help='Requests for this namespace should never be proxied. Can be specified multiple times.')
+    parser.add_argument('--log-headers', action='store_true', env_var='GALACTORY_LOG_HEADERS', help='Log the headers of every request (DEBUG level only).')
+    parser.add_argument('--log-body', action='store_true', env_var='GALACTORY_LOG_BODY', help='Log the body of every request (DEBUG level only).')
+    parser.add_argument('--proxy-upstream', type=lambda x: str(x).rstrip('/') + '/', env_var='GALACTORY_PROXY_UPSTREAM', help='If set, then find, pull and cache results from the specified galaxy server in addition to local.')
+    parser.add_argument('-npns', '--no-proxy-namespace', action='append', default=[], env_var='GALACTORY_NO_PROXY_NAMESPACE', help='Requests for this namespace should never be proxied. Can be specified multiple times.')
     args = parser.parse_args()
 
     logging.basicConfig(filename=args.log_file, level=args.log_level)
