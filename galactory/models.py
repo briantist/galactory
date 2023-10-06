a247fa9da6633f1dc79667a374e73cc93632ac13
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-10-06 12:13:20 -0400

    add new models and a few basic tests

---
+++ galactory/models.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+# (c) 2023 Brian Scholer (@briantist)
+
+import json
+
+import typing as t
+from typing import ValuesView
+
+from semver import VersionInfo
+from collections import UserDict
+from dataclasses import dataclass
+from functools import total_ordering, cached_property
+from datetime import datetime
+
+from artifactory import ArtifactoryPath
+
+
+@total_ordering
+@dataclass(eq=False, order=False)
+class CollectionData:
+    collection_info: dict
+    created_datetime: datetime
+    modified_datetime: datetime
+    namespace: str
+    name: str
+    filename: str
+    sha256: str
+    size: int
+    mime_type: str
+    version: str
+
+    @classmethod
+    def from_artifactory_path(cls, *, path: ArtifactoryPath, properties: dict, stat: dict):
+        collection_info = json.loads(properties['collection_info'][0])
+
+        return cls(
+            collection_info=collection_info,
+            created_datetime=stat.ctime,
+            modified_datetime=stat.mtime,
+            namespace=properties['namespace'][0],
+            name=properties['name'][0],
+            filename=path.name,
+            sha256=stat.sha256,
+            size=stat.size,
+            mime_type=stat.mime_type,
+            version=properties['version'][0],
+        )
+
+    def __eq__(self, other) -> bool:
+        # All of the comparable information is in the collection
+        # tarball itself, so this should be a nice fast way of
+        # comparing equality, without computing the cached_properties.
+        try:
+            return self.sha256 == other.sha256
+        except AttributeError:
+            # Allow for comparing directly with VersionInfo objects.
+            if isinstance(other, VersionInfo):
+                return self.semver == other
+            return NotImplemented
+
+    def __lt__(self, other) -> bool:
+        this_is_prerelease = self.is_prerelease
+        this_version = self.semver
+
+        if isinstance(other, VersionInfo):
+            other_is_prerelease = other.prerelease is not None
+            other_version = other
+        elif isinstance(other, CollectionData):
+            other_is_prerelease = other.is_prerelease
+            other_version = other.semver
+            if self.name != other.name or self.namespace != other.namespace:
+                return False
+        else:
+            return NotImplemented
+
+        # Both objects are prerelease versions, or both are not, so compare normally.
+        if this_is_prerelease == other_is_prerelease:
+            return this_version < other_version
+
+        # One of the objects is not a prerelease and the other is.
+        # If self is a prerelease, then it is necessarily < the other.
+        return this_is_prerelease
+
+    @cached_property
+    def fqcn(self) -> str:
+        return f"{self.namespace}.{self.name}"
+
+    @cached_property
+    def created(self) -> str:
+        return self.created_datetime.isoformat()
+
+    @cached_property
+    def modified(self) -> str:
+        return self.modified_datetime.isoformat()
+
+    @cached_property
+    def semver(self) -> VersionInfo:
+        return VersionInfo.parse(self.version)
+
+    @cached_property
+    def is_prerelease(self) -> bool:
+        return self.semver.prerelease is not None
+
+
+class CollectionGroup(UserDict):
+    """
+    A dict-like object where the keys are collection names,
+    and the values are CollectionData objects.
+    """
+    latest: CollectionData = None
+
+    def __init__(self, *, namespace: str, name: str):
+        self.namespace = namespace
+        self.name = name
+        super().__init__()
+
+    @classmethod
+    def from_collection(cls, collection: CollectionData):
+        instance = cls(namespace=collection.namespace, name=collection.name)
+        instance.add(collection)
+        return instance
+
+    @property
+    def versions(self) -> dict:
+        return self.data
+
+    @cached_property
+    def fqcn(self) -> str:
+        return f"{self.namespace}.{self.name}"
+
+    def add(self, collection: CollectionData) -> None:
+        self[collection.semver] = collection
+
+    def _get_key(self, key: t.Union[str, VersionInfo], *, raises: bool = True) -> VersionInfo:
+        if isinstance(key, VersionInfo):
+            return key
+        elif isinstance(key, str):
+            return VersionInfo.parse(key)
+        else:
+            if raises:
+                raise TypeError("Only valid semantic versions can be used as keys.")
+            else:
+                return key
+
+    def __getitem__(self, key: t.Union[str, VersionInfo]) -> CollectionData:
+        return super().__getitem__(self._get_key(key))
+
+    def __setitem__(self, key: t.Union[str, VersionInfo], item: CollectionData) -> None:
+        if not isinstance(item, CollectionData):
+            raise TypeError("Values must be of type CollectionData.")
+
+        if self.name != item.name or self.namespace != item.namespace:
+            raise ValueError(f"Attempted to add collection '{item.namespace}.{item.name}' to group for '{self.namespace}.{self.name}'.")
+
+        if self.latest is None or item > self.latest:
+            self.latest = item
+
+        return super().__setitem__(self._get_key(key), item)
+
+    def __delitem__(self, key: t.Union[str, VersionInfo]) -> None:
+        return super().__delitem__(self._get_key(key))
+
+    def __contains__(self, key: object) -> bool:
+        return super().__contains__(self._get_key(key, raises=False))
+
+    # re-define for the type hints
+    def values(self) -> ValuesView[CollectionData]:
+        return super().values()
+
+
+class CollectionCollection(UserDict):
+    """
+    A Dict[str, CollectionGroup] object where the keys are FQCNs.
+    """
+    @classmethod
+    def from_collections(cls, collections: t.Iterable[CollectionData]):
+        instance = cls()
+        for collection in collections:
+            instance.add(collection)
+        return instance
+
+    def add(self, collection: CollectionData) -> None:
+        if collection.fqcn in self.data:
+            self.data[collection.fqcn].add(collection)
+        else:
+            self.data[collection.fqcn] = CollectionGroup.from_collection(collection)
+
+    # re-define for the type hints
+    def values(self) -> ValuesView[CollectionGroup]:
+        return super().values()
---
+++ tests/unit/models/test_collectiondata.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+# (c) 2023 Brian Scholer (@briantist)
+
+import pytest
+
+from datetime import datetime, timedelta, timezone
+import semver
+
+from galactory.models import CollectionData
+
+
+@pytest.mark.parametrize('collection_info', [{}])
+@pytest.mark.parametrize('created_datetime', [datetime.now(timezone.utc)])
+@pytest.mark.parametrize('modified_datetime', [datetime.now(timezone.utc)])
+@pytest.mark.parametrize('filename', ['fake-file'])
+@pytest.mark.parametrize('mime_type', ['fake-mime'])
+@pytest.mark.parametrize('name', ['collection_name'])
+@pytest.mark.parametrize('namespace', ['a_namespace'])
+@pytest.mark.parametrize('sha256', ['m-m-m-my-sha-'])
+@pytest.mark.parametrize('size', [-1, 0, 1])
+@pytest.mark.parametrize('version', ['invalid', '1.2.3', '1.2.3-dev0'])
+def test_collectiondata_init(
+    collection_info,
+    created_datetime,
+    modified_datetime,
+    filename,
+    mime_type,
+    name,
+    namespace,
+    sha256,
+    size,
+    version,
+):
+    data = CollectionData(
+        collection_info=collection_info,
+        created_datetime=created_datetime,
+        modified_datetime=modified_datetime,
+        filename=filename,
+        mime_type=mime_type,
+        name=name,
+        namespace=namespace,
+        sha256=sha256,
+        size=size,
+        version=version,
+    )
+
+    assert isinstance(data, CollectionData)
+    assert data.created_datetime.utcoffset() == timedelta(0)
+    assert data.modified_datetime.utcoffset() == timedelta(0)
+    assert datetime.fromisoformat(data.created) == data.created_datetime
+    assert datetime.fromisoformat(data.modified) == data.modified_datetime
+    assert data.fqcn == f"{data.namespace}.{data.name}"
+    assert data.version == version
+    try:
+        ver = semver.VersionInfo.parse(version)
+    except ValueError:
+        with pytest.raises(ValueError, match=rf"^{version} is not valid SemVer string"):
+            _ = data.semver
+    else:
+        assert isinstance(data.semver, semver.VersionInfo)
+        assert semver.VersionInfo.parse(data.version) == data.semver
+        assert data.semver == ver
+
+
+_COLLECTION_INFO = [
+    ('fake', 'one', '0.1.0', 'A'),
+    ('fake', 'one', '0.1.0', 'B'),
+    ('fake', 'one', '0.1.0-dev0', 'A'),
+    ('fake', 'two', '2.0.0', 'C'),
+    ('jake', 'two', '2.0.0', 'D'),
+]
+
+
+@pytest.mark.parametrize(
+    ('namespace1', 'name1', 'version1', 'sha256_1'),
+    _COLLECTION_INFO,
+)
+@pytest.mark.parametrize(
+    ('namespace2', 'name2', 'version2', 'sha256_2'),
+    _COLLECTION_INFO,
+)
+def test_collectiondata_compare_collections(
+    namespace1, name1, version1, sha256_1,
+    namespace2, name2, version2, sha256_2,
+):
+    _common = dict(
+        collection_info={},
+        created_datetime=datetime.now(timezone.utc),
+        modified_datetime=datetime.now(timezone.utc),
+        filename='fname',
+        mime_type='mime',
+        size=0,
+    )
+
+    colA = CollectionData(namespace=namespace1, name=name1, version=version1, sha256=sha256_1, **_common)
+    colB = CollectionData(namespace=namespace2, name=name2, version=version2, sha256=sha256_2, **_common)
+
+    verA = semver.VersionInfo.parse(version1)
+    verB = semver.VersionInfo.parse(version2)
+
+    assert (colA == colB) == (sha256_1 == sha256_2)
+
+    if colA != colB:
+        if name1 != name2 or namespace1 != namespace2:
+            assert (colA < colB) == False
+        elif verA.prerelease is None == verB.prerelease is None:
+            assert (colA < colB) == (verA < verB)
+        else:
+            assert (colA < colB) == (verA.prerelease is not None)
+
+
+@pytest.mark.parametrize(
+    ('namespace1', 'name1', 'version1', 'sha256_1'),
+    _COLLECTION_INFO,
+)
+@pytest.mark.parametrize(
+    'other_version',
+    list(map(lambda x: x[2], _COLLECTION_INFO)),
+)
+def test_collectiondata_compare_versioninfo(
+    namespace1, name1, version1, sha256_1,
+    other_version,
+):
+    _common = dict(
+        collection_info={},
+        created_datetime=datetime.now(timezone.utc),
+        modified_datetime=datetime.now(timezone.utc),
+        filename='fname',
+        mime_type='mime',
+        size=0,
+    )
+
+    col = CollectionData(namespace=namespace1, name=name1, version=version1, sha256=sha256_1, **_common)
+
+    ver = semver.VersionInfo.parse(other_version)
+
+    if col.is_prerelease == (ver.prerelease is not None):
+        assert (col < ver) == (col.semver < ver)
+    else:
+        assert (col < ver) == (col.is_prerelease)
