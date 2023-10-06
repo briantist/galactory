59e23c64bf283c4d6121b8637a98c3d36df1e20f
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-10-06 15:20:01 -0400

    better download URL for upstream results

--- galactory/api/v2/collections.py
+++ galactory/api/v2/collections.py
@@ -229,6 +229,12 @@ def version(namespace, collection, version):
         if upstream and (not no_proxy or namespace not in no_proxy):
             proxy = ProxyUpstream(repository, upstream, cache_read, cache_write, cache_minutes)
             upstream_result = proxy.proxy(request)
+            url_for(
+                'download.download',
+                filename=upstream_result['artifact']['filename'],
+                _external=True,
+                _scheme=scheme,
+            ),
             return upstream_result
         else:
             abort(C.HTTP_NOT_FOUND)
