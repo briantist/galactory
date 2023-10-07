897145af3662bf0ce323162fc9fb2d483eb0e6c4
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-10-07 14:46:37 -0400

    make API versions configurable

--- galactory/api/__init__.py
+++ galactory/api/__init__.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 # (c) 2022 Brian Scholer (@briantist)
 
-from flask import Blueprint
+from flask import Blueprint, Flask
 
-from .v2 import bp as v2
-from .v3 import bp as v3
 
-API_RESPONSE = {
-    'available_versions': {
-        'v2': 'v2/',
-        'v3': 'v3/',
-    },
-    'current_version': 'v2', # This field doesn't exist in the v3 output anyway.
-    'description': 'GALAXY REST API',
-}
+def create_blueprint(app: Flask):
+    api_version = app.config['API_VERSION']
+    response = {
+        'available_versions': {},
+        'description': 'GALAXY REST API',
+    }
 
-bp = Blueprint('api', __name__, url_prefix='/api')
-bp.register_blueprint(v2)
-bp.register_blueprint(v3)
+    bp = Blueprint('api', __name__, url_prefix='/api')
 
-@bp.route('')
-@bp.route('/')
-def api():
-    return API_RESPONSE
+    if api_version is None or 'v3' in api_version:
+        app.logger.info(f"V3 - {api_version}")
+        response['available_versions']['v3'] = 'v3/'
+        from .v3 import bp as v3
+        bp.register_blueprint(v3)
+
+    if api_version is None or 'v2' in api_version:
+        app.logger.info(f"V2 - {api_version}")
+        response['available_versions']['v2'] = 'v2/'
+        response['current_version'] = 'v2' # This field doesn't exist in the v3 output.
+        from .v2 import bp as v2
+        bp.register_blueprint(v2)
+
+    @bp.route('')
+    @bp.route('/', endpoint='api')
+    def api():
+        return response
+
+    return bp
