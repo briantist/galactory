f4d9cbed89519608688aef4465e0c9d6603bbcf9
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-10-08 13:26:24 -0400

    nit: not f-strings

--- galactory/api/__init__.py
+++ galactory/api/__init__.py
@@ -14,13 +14,13 @@ def create_blueprint(app: Flask):
     bp = Blueprint('api', __name__, url_prefix='/api')
 
     if api_version is None or 'v3' in api_version:
-        app.logger.info(f"Registering Galaxy API v3")
+        app.logger.info("Registering Galaxy API v3")
         response['available_versions']['v3'] = 'v3/'
         from .v3 import bp as v3
         bp.register_blueprint(v3)
 
     if api_version is None or 'v2' in api_version:
-        app.logger.info(f"Registering Galaxy API v2")
+        app.logger.info("Registering Galaxy API v2")
         response['available_versions']['v2'] = 'v2/'
         response['current_version'] = 'v2' # This field doesn't exist in the v3 output.
         from .v2 import bp as v2
